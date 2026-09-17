e97f3eb31541e08088be5f0e16456eec7ffa0ca9
Vangala Priyanka <23A81A05C9@sves.org.in>
2026-09-17 14:15:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (47.37%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=0;
+        int j=0;
+        int k=0;
+        int temp[]=new int[m+n];
+        while(i<m&&j<n)
+        {
+            if(nums1[i]<=nums2[j])
+            {
+                 temp[k++]=nums1[i];
+                 i++;
+
+            }
+            else
+            {
+                temp[k++]=nums2[j];
+                j++;
+            }
+        }
+        while(i<m)
+        {
+            temp[k++]=nums1[i];
+            i++;
+        }
+        while(j<n)
+        {
+            temp[k++]=nums2[j];
+            j++;
+        }
+        for(int x=0;x<m+n;x++)
+        {
+            nums1[x]=temp[x];
+        }
+    }
+}
