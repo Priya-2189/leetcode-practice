5a7755a4df3c1cec8cd11f800a87f221a4662ac2
Vangala Priyanka <23A81A05C9@sves.org.in>
2026-09-17 16:08:28 +0530

    Time: 784 ms (18.26%) | Memory: 80 MB (15.33%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countPrimes(int n) {
+        int c=0;
+        if(n<=2)
+        {
+            return 0;
+        }
+        boolean isprime[]=new boolean[n];
+
+        for(int i=2;i<n;i++)
+        {
+            isprime[i]=true;
+        }
+        for(int i=2;i*i<n;i++)
+        {
+            if(isprime[i])
+            {
+                for(int j=i*i;j<n;j+=i)
+                {
+                    isprime[j]=false;
+                }
+            }
+        }
+        for(int i=2;i<n;i++)
+        {
+            if(isprime[i])
+            {
+                c++;
+            }
+        }
+        return c;
+        
+    }
+}
