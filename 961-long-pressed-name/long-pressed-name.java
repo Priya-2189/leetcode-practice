f634371b04de696cbcc616413d505b17e5d07e32
Vangala Priyanka <23A81A05C9@sves.org.in>
2026-09-20 15:15:36 +0530

    Time: 1 ms (87.80%) | Memory: 43.1 MB (19.43%) - LeetSync

---
+++ 961-long-pressed-name/long-pressed-name.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        int i=0;
+        int j=0;
+        int n=name.length();
+        int m=typed.length();
+        while(j<typed.length())
+        {
+            if(i<n&&name.charAt(i)==typed.charAt(j))
+            {
+                i++;
+                j++;
+            }
+            else if(j>0&&typed.charAt(j)==typed.charAt(j-1))
+            {
+                j++;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        return i==name.length();
+    }
+}
